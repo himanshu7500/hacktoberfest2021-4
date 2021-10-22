d08155f03db4b8c2623069ebbff041c01bfac4cc
Aman Chandel <amaniiitu32@gmail.com>
2021-10-22 14:52:41 +0530

    Add files via upload

---
+++ subarrays.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(int i=0;i<n;i++){
+        int current = 0;
+        for(int j=i;j<n;j++){
+            current+=a[j];
+            cout<<current<<endl;
+        }
+    }
+    return 0;           //Sum of all subarrays
+}
